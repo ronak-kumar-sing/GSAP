5b2e4205bdab5a8b23cbef508dd51b48ba3eb7ae
Ronak Kumar <roankkumar@192.168.1.4>
2024-11-04 20:11:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let t1 = gsap.timeline()
+
+t1.from('#box1 h1', {
+  y: -50,
+  opacity: 0,
+  delay: 0.4,
+  duration: 0.8,
+  stagger: 0.2,
+  ease: 'power2.out',
+})
+
+t1.from('#box2 h1', {
+  x: -100,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.2,
+  ease: 'power2.out',
+})
+
+t1.from('#img img', {
+  x: -100,
+  rotate: 20,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.2,
+  ease: 'power3.out',
+})
